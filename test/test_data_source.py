4774c2ec584054887417fb40ceb95ca930612875
walkacross <yujiangallen@126.com>
2021-01-02 19:07:38 +0800

    fix unittest CI

--- test/test_data_source.py
+++ test/test_data_source.py
@@ -9,7 +9,7 @@ import unittest
 class TestDataSource(unittest.TestCase):
     def test_data_from_data_utils(self):
         feature_df, price_s = create_toy_feature(order_book_ids_number=2, feature_number=3, start="2020-01-01", end="2020-01-11",random_seed=555)
-
+        print(price_s)
         data_source = DataSource(feature_df=feature_df, price_s=price_s)
     
         #availabel_dts
@@ -25,7 +25,7 @@ class TestDataSource(unittest.TestCase):
 
     
         #get_availabel_order_book_ids
-        order_book_ids = data_source.get_availabel_order_book_ids()
+        order_book_ids = data_source.get_available_order_book_ids()
         
         forth_dt_expected = pd.Timestamp("2020-01-04")
         forth_dt = availabel_dts[3]
@@ -36,7 +36,7 @@ class TestDataSource(unittest.TestCase):
             
         #get last price
         last_price = data_source.get_last_price(order_book_id=order_book_ids[0], dt=forth_dt_expected)
-        last_price_expected = 36.34
+        last_price_expected = 20.11
         self.assertEqual(last_price, last_price_expected)
     
 if __name__ == "__main__":
